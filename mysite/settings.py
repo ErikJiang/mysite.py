fd788698fe0b3c9a787b5b5156d4b7e9e9e10d1f
JiangInk <JiangInk@foxmail.com>
2018-04-17 22:29:50 +0800

    generic views

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mysite',
         'USER': 'test',
-        'PASSWORD': '123456789',
+        'PASSWORD': 'jiangink',
         'HOST': '127.0.0.1'
     }
 }
--- polls/urls.py
+++ polls/urls.py
@@ -6,11 +6,11 @@ from . import views
 app_name = 'polls'  # 设置url命名空间
 urlpatterns = [
     # url(r'^$', views.index, name='index'),
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     # url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     # url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     # url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 
